45ffa16e47a878a4924a4d3891d9d181fcf9cb63
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-03-12 14:57:06 -0400

    finish

--- Day-2 Countdown Timer/countdown.js
+++ Day-2 Countdown Timer/countdown.js
@@ -6,6 +6,12 @@
     const HOUR = MINUTE * 60;
     const DAY = HOUR * 24;
 
+    // create function setInnerText() take id and text in the params
+    function setInnerText(id, text) {
+        const element = document.getElementById(id);
+        element.innerText = text;
+    }
+
     // create countdown function 
     // first the function is going to show the current time
     function countDown() {
@@ -17,15 +23,28 @@
 
         // console.log(tillNewYear);
 
-        const daysEle = document.getElementById("days");
-        daysEle.innerText = "Hello";
+        // const days = document.getElementById("days");
+        // days.innerText = Math.floor(tillNewYear / DAY);
+        setInnerText("days", Math.floor(tillNewYear / DAY));
 
-        console.log(daysEle);
+        // const hours = document.getElementById("hours");
+        // hours.innerText = Math.floor(tillNewYear % DAY / HOUR);
+        setInnerText("hours", Math.floor(tillNewYear % DAY / HOUR));
+
+        // const minutes = document.getElementById("minutes");
+        // minutes.innerText = Math.floor(tillNewYear % HOUR / MINUTE);
+        setInnerText("minutes", Math.floor(tillNewYear % HOUR / MINUTE));
+
+        // const seconds = document.getElementById("seconds");
+        // seconds.innerText = Math.floor(tillNewYear % MINUTE / SECOND);
+        setInnerText("seconds", Math.floor(tillNewYear % MINUTE / SECOND));
 
     }
 
     function run() {
         countDown();
+        //  to run the countdown function when run function called it'll run every milliseconds as we set the SECOND at the beginning. 
+        setInterval(countDown, SECOND);
 
     }
     // call back function
