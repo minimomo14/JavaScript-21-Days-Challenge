89354823f798146e5bba2605c4f261cf255a9f26
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-03-12 18:35:09 -0400

    finish

--- Day-4 Eye Rolling/script.js
+++ Day-4 Eye Rolling/script.js
@@ -2,16 +2,28 @@
     // create run function
     function run() {
         const bodyElem = document.querySelector("body");
-        const eyeElem = document.querySelectorAll("eye");
+        const eyeElem = document.querySelectorAll(".eye");
 
-        function onMouseMove() {
+        function onMouseMove({pageX, pageY}) {
             // check if the mouse moving
-            console.log("Moved")
+            // console.log("Moved")
+            eyeElem.forEach((eyeElem) => {
+               const {left, top} = eyeElem.getBoundingClientRect();
+
+               const eyeCenterX = left + eyeElem.offsetWidth / 2;
+               const eyeCenterY = top + eyeElem.offsetHeight / 2;
+            // calculate the radiant 
+                const radian = Math.atan2(pageX - eyeCenterX, pageY - eyeCenterY);
+            // then to find the angle (degree)
+                const angle = radian * 180 / Math.PI * -1;
+                console.log(angle)
+            // use the angle value to set movement of the eyeballs
+                eyeElem.style.transform = `rotate(${angle}deg)`;
+            });
         }
 
         // for the body element addEventListener() to follow the move
-        bodyElem.addEventListener("mousemove", onMouseMove)
-
+        bodyElem.addEventListener("mousemove", onMouseMove);
     }
     run();
 })();
