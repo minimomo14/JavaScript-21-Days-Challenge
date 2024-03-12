d9d01f3a52a8cadb8199525bc65d3cde0f3e0b9e
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-03-12 16:12:03 -0400

    finish lecture day 3

---
+++ Day-3 Async Await/script.js
@@ -0,0 +1,69 @@
+(() => {
+
+    // 1. How Asynchronous code works in JavaScript?
+    // function simulateAsyncAPI(text, timeout) {
+    //     setTimeout(() => console.log(text), timeout);
+    // }
+
+    // simulateAsyncAPI("A", 1000);
+
+
+    // 2. Callback
+    // function simulateAsyncAPI(text, timeout, callback) {
+    //     setTimeout(() => {
+    //         console.log(text)
+    //         callback();
+    //     }, timeout)
+    // }
+    // // callback hell
+    // simulateAsyncAPI("A", 1000, () => {console.log("Callback");
+    //  simulateAsyncAPI("B", 500, () => {
+    //     simulateAsyncAPI("c", 100);
+    //  });
+    // })
+
+
+    // 3.Promise
+    // function simulateAsyncAPI(text, timeout) {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             console.log(text);
+    //             resolve();
+    //         }, timeout)
+    //     })
+    // }
+
+    // simulateAsyncAPI("A", 1000)
+    // .then(() => {
+    //     return simulateAsyncAPI("B", 500);
+    // })
+    // .then(() => {
+    //     return simulateAsyncAPI("C", 100);
+    // })
+    // .catch((error) => {
+    //     console.error(error);
+    // })
+
+    // 4. Async/ Await
+ function simulateAsyncAPI(text, timeout) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+               if (text === "B") return reject("B got rejected!")
+               console.log(text);
+                resolve();
+            }, timeout)
+        })
+    }
+async function run() {
+    try{
+        await simulateAsyncAPI("A", 1000);
+        await simulateAsyncAPI("B", 500);
+        await simulateAsyncAPI("C", 100);
+    } catch(error) {
+        console.error(error);
+    }
+   
+}
+run();
+
+})();
