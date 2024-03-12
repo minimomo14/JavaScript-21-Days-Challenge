c487312ccc38ed78be3cfe9229ec558fe3c5d05f
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-03-12 14:35:53 -0400

    initial commit

--- Day-2 Countdown Timer/countdown.js
+++ Day-2 Countdown Timer/countdown.js
@@ -1,3 +1,33 @@
 (() => {
+// create function that run the website
+    // set up the timer value
+    const SECOND = 1000;
+    const MINUTE = SECOND * 60;
+    const HOUR = MINUTE * 60;
+    const DAY = HOUR * 24;
 
+    // create countdown function 
+    // first the function is going to show the current time
+    function countDown() {
+        const currentTime = new Date().getTime();
+        // set new year eve date (time stamp) and time (24 hours clock")
+        const newYearEve = new Date("December 31, 2024 23:59:59").getTime();
+        // create new variable to calculate time left till new year
+        const tillNewYear = newYearEve - currentTime;
+
+        // console.log(tillNewYear);
+
+        const daysEle = document.getElementById("days");
+        daysEle.innerText = "Hello";
+
+        console.log(daysEle);
+
+    }
+
+    function run() {
+        countDown();
+
+    }
+    // call back function
+    run();
 })();
